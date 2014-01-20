518fedeb55ffa1f484e1ad67d1c9557b06cf6d0a
Raul Sanchez <raul@um.es>
2014-01-20 14:14:29 +0100

    restart app after deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,6 +23,7 @@ namespace :deploy do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
       # execute :touch, release_path.join('tmp/restart.txt')
+      execute :sudo, '/etc/init.d/openvdi restart'
     end
   end
 
