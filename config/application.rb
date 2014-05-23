90839cd2db77c83ec82da7e6f5401ff52d5244ef
Raul Sanchez <raul@um.es>
2014-05-23 18:42:11 +0200

    changed config.action_mailer.default_url_options to adecuate to hostname automatically

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module Broker
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.action_mailer.default_url_options = { host: 'openvdi.com' }
+    config.action_mailer.default_url_options = { host: `hostname` }
     config.i18n.enforce_available_locales = true
 
     config.generators.stylesheets = false
