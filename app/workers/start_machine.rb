1cde6902afca3c0f71ac6833bba038012cabbe0e
Raul Sanchez <raul@um.es>
2014-05-28 18:55:10 +0200

    only create new machine if not enough running machines waiting

--- app/workers/start_machine.rb
+++ app/workers/start_machine.rb
@@ -1,9 +1,11 @@
 class StartMachine
   include Sidekiq::Worker
     def perform(machine_id)
+      if (Machine.paused(Machine.find(machine_id).image).count < Machine.find(machine_id).image.number_of_instances)
         clave = (0...16).map { ('a'..'z').to_a[rand(26)] }.join
         machine = Machine.find_by_id(machine_id)
         machine.cloud_create
+      end
 #        sleep 10
 #        machine.pause
     end
